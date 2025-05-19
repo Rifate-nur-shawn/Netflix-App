a5412a6b1227db3531bab34b361ac7d30cbcb0c6
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-05-19 13:43:27 +0600

    fix-error

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import React from 'react'
-import Home from './pages/Home/Home'
-import { Routes, Route,useNavigate } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import Player from './pages/Player/Player'
-import { onAuthStateChanged } from 'firebase/auth'
-import { useEffect } from 'react'
-import { auth } from './firebase'
-import { ToastContainer, toast } from 'react-toastify';
+import React from 'react';
+import Home from './pages/Home/Home';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Player from './pages/Player/Player';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
 const App = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
   useEffect(() => {
-    onAuthStateChanged(auth,async (user) => {
-      if (user) {
-        console.log("logged in")
-        navigate('/')	
-      }else{
-        console.log("not logged in")
-        navigate('/login');
-      }
-    })
-  }, [])
+    onAuthStateChanged(auth, (user) => {
+      setTimeout(() => {
+        if (user) {
+          console.log("logged in");
+          navigate('/');
+        } else {
+          console.log("not logged in");
+          navigate('/login');
+        }
+      }, 0);
+    });
+  }, []);
+
   return (
     <div>
-       <ToastContainer theme='dark'/>
+      <ToastContainer theme='dark' />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/player/:id' element={<Player />}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/player/:id' element={<Player />} />
       </Routes>
-      
     </div>
-  )
+  );
 };
 
 export default App;
